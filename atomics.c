d976884f131e37a12f70ffbc4b152e3f70a0e172
will <william.calliari@gmail.com>
2020-06-03 08:01:48 +0200

    updated atomics.c

--- atomics.c
+++ atomics.c
@@ -1,6 +1,6 @@
 
 /*
-Declaration: _Atomic type name; 
+Declaration: _Atomic type name;
 
 Built-in Function: void __atomic_store (type *ptr, type *val, int memorder)
 It stores the value of *val into *ptr.
@@ -24,7 +24,7 @@ of the uintptr_t type. That is, they are not scaled by the size of the type to w
 #include <stdlib.h>
 #include <stdio.h>
 
-_Atomic int x; 
+_Atomic int x;
 
 int main() {
     int value = 22;
@@ -37,7 +37,30 @@ int main() {
 
     // load
     __atomic_load(&x, &value, 0);
-    
+
     printf("value of x = %d\n", value);
     return EXIT_SUCCESS;
 }
+
+// Alternative:
+#include <stdlib.h>
+#include <stdatomic.h>
+
+atomic_int x;
+
+int main(int argc, char const *argv[]) {
+    //initialize
+    x = 5;
+
+    //increment
+    x++;
+
+    //decrement
+    x--;
+
+    //load
+    int value = x;
+
+    return 0;
+}
+//
